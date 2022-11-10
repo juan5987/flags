5df133477b56eab25adaba2e12296867475e53bb
Juan5987 <juan.machado.5987@gmail.com>
2022-11-10 16:28:46 +0100

    working on signup

--- src/components/Signup/Signup.tsx
+++ src/components/Signup/Signup.tsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import './signup.sass';
 
 const Signup = () => {
+  const [formValues, setFormValues] = useState({
+    username: '',
+    email: '',
+    email_confirm: '',
+    password: '',
+    password_confirm: '',
+  });
+
+  const [error, setError] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(formValues);
+  };
+
+  const handleChange = (e: any) => {
+    setFormValues({
+      ...formValues,
+      [e.target.id]: e.target.value,
+    });
+  };
+
   return (
     <div className='signup'>
       <Header />
       <h2 className='signup__title'>Création d'un compte</h2>
-      <form className='signup__form'>
+      <form className='signup__form' onSubmit={handleSubmit}>
         <div className='signup__form__element'>
           <label className='signup__form__element__label' htmlFor='username'>
             Nom d'utilisateur
@@ -17,7 +39,15 @@ const Signup = () => {
             className='signup__form__element__input'
             type='text'
             id='username'
+            onChange={handleChange}
+            value={formValues.username}
+            autoComplete='username'
           />
+          {error && (
+            <span className='signup__form__element__error'>
+              Ce nom d'utilisateur est déjà utilisé
+            </span>
+          )}
         </div>
         <div className='signup__form__element'>
           <label className='signup__form__element__label' htmlFor='email'>
@@ -27,7 +57,15 @@ const Signup = () => {
             className='signup__form__element__input'
             type='email'
             id='email'
+            onChange={handleChange}
+            value={formValues.email}
+            autoComplete='email'
           />
+          {error && (
+            <span className='signup__form__element__error'>
+              Cette adresse email est déjà utilisée
+            </span>
+          )}
         </div>
         <div className='signup__form__element'>
           <label
@@ -40,7 +78,15 @@ const Signup = () => {
             type='email'
             className='signup__form__element__input'
             id='email_confirm'
+            onChange={handleChange}
+            value={formValues.email_confirm}
+            autoComplete='email'
           />
+          {error && (
+            <span className='signup__form__element__error'>
+              L'adresse mail ne correspond pas
+            </span>
+          )}
         </div>
         <div className='signup__form__element'>
           <label className='signup__form__element__label' htmlFor='password'>
@@ -50,7 +96,15 @@ const Signup = () => {
             className='signup__form__element__input'
             type='password'
             id='password'
+            onChange={handleChange}
+            value={formValues.password}
+            autoComplete='new-password'
           />
+          {error && (
+            <span className='signup__form__element__error'>
+              Le mot de passe doit contenir au moins 8 caractères
+            </span>
+          )}
         </div>
         <div className='signup__form__element'>
           <label
@@ -63,7 +117,15 @@ const Signup = () => {
             type='password'
             className='signup__form__element__input'
             id='password_confirm'
+            onChange={handleChange}
+            value={formValues.password_confirm}
+            autoComplete='new-password'
           />
+          {error && (
+            <span className='signup__form__element__error'>
+              Le mot de passe doit contenir au moins 8 caractères
+            </span>
+          )}
         </div>
         <button className='signup__form__submit' type='submit'>
           Valider
