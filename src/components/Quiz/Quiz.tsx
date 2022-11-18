4ce9dd7e7646a662974d4b2e890ad2a82f940c4c
Juan5987 <juan.machado.5987@gmail.com>
2022-11-18 15:30:46 +0100

    styling

--- src/components/Quiz/Quiz.tsx
+++ src/components/Quiz/Quiz.tsx
@@ -100,123 +100,135 @@ const Quiz = () => {
   return (
     <div className='quiz'>
       <div className='quiz__background'></div>
-      <div className='quiz__mobile'>
-        <div className='quiz__mobile__score'>
-          <h2 className='quiz__mobile__score__label'>Score</h2>
-          <span className='quiz__mobile__score__value'>{score}</span>
-        </div>
-        <div className='quiz__mobile__timer'>
-          <h2 className='quiz__mobile__timer__label'>Temps restant</h2>
-          <span className='quiz__mobile__timer__value'>{timer}</span>
-        </div>
-      </div>
-      <div className='quiz__top'>
-        <div className='quiz__top__left'>
-          <h2 className='quiz__top_title'>Score</h2>
-          <span className='quiz__top__score'>{score}</span>
-        </div>
-        <div>
-          <Link className='quiz__quit' to='/'>
-            quitter
-          </Link>
-          {!isLoading ? (
-            <img
-              className='quiz__top__flag'
-              src={currentFlag}
-              alt='drapeau'
-              height={300}
-            />
-          ) : (
-            <h2 className='quiz__top__loading'>Sélection du drapeau...</h2>
-          )}
-        </div>
-        <div className='quiz__top__right'>
-          <h2 className='quiz__top_title'>Temps restant</h2>
-          <span className='quiz__top__score'>{timer}</span>
-        </div>
-      </div>
-
-      <form className='quiz__answer' onSubmit={handleSubmit}>
-        {
-          <input
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setAnswer(e.target.value)
-            }
-            className='quiz__answer__letter'
-            type='text'
-            ref={inputRef}
-            placeholder='Réponse'
-            value={answer}
-          />
-        }
-        <div className='quiz__buttons'>
-          <button className='quiz__buttons__button' onClick={handlePass}>
-            Passer
-          </button>
-          <button className='quiz__buttons__button'>Valider</button>
-        </div>
-      </form>
-      {showResult && (
-        <div className='quiz__result'>
-          <div className='quiz__result__content'>
-            {result && (
-              <>
-                <img
-                  className='quiz__result__content__check'
-                  src={check}
-                  alt='check logo'
-                />
-                <h3 className='quiz__result__content__correct'>
-                  Félicitations !
-                </h3>
-                <span className='quiz__result__content__text'>
-                  La réponse est:
-                </span>
-              </>
-            )}
-            {!result && (
-              <>
-                <div className='quiz__result__content__cross'>x</div>
-                <h3 className='quiz__result__content__incorrect'>Dommage</h3>
-                <span className='quiz__result__content__text'>
-                  La réponse était:
-                </span>
-              </>
-            )}
-            <span className='quiz__result__content__solution'>{solution}</span>
-            <button
-              className='quiz__result__content__next'
-              onClick={handleNextFlag}
-            >
-              Drapeau suivant
-              <img src={chevron_right} alt='chevron' />
-            </button>
-          </div>
-        </div>
-      )}
-      {gameOver && (
-        <div className='quiz__gameover'>
-          <div className='quiz__gameover__content'>
-            <h2 className='quiz__gameover__content__title'>partie terminée</h2>
-            <span className='quiz__gameover__content__score'>
-              Votre score est: {score}
-            </span>
-            <div className='quiz__gameover__content__buttons'>
-              <button
-                className='quiz__gameover__content__buttons__play'
-                onClick={handlePlayAgain}
-              >
-                Rejouer
-              </button>
-              <Link className='quiz__gameover__content__buttons__quit' to='/'>
-                Quitter
-              </Link>
+      {isLoading ? (
+        <h2 className='quiz__top__loading'>Chargement de la partie...</h2>
+      ) : (
+        <>
+          <div className='quiz__mobile'>
+            <div className='quiz__mobile__score'>
+              <h2 className='quiz__mobile__score__label'>Score</h2>
+              <span className='quiz__mobile__score__value'>{score}</span>
+            </div>
+            <div className='quiz__mobile__timer'>
+              <h2 className='quiz__mobile__timer__label'>Temps restant</h2>
+              <span className='quiz__mobile__timer__value'>{timer}</span>
             </div>
           </div>
-        </div>
-      )}
+          <div className='quiz__top'>
+            <div className='quiz__top__left'>
+              <h2 className='quiz__top_title'>Score</h2>
+              <span className='quiz__top__score'>{score}</span>
+            </div>
+            <div>
+              <Link className='quiz__quit' to='/'>
+                quitter
+              </Link>
 
-      <Footer />
+              <img
+                className='quiz__top__flag'
+                src={currentFlag}
+                alt='drapeau'
+                height={300}
+              />
+            </div>
+            <div className='quiz__top__right'>
+              <h2 className='quiz__top_title'>Temps restant</h2>
+              <span className='quiz__top__score'>{timer}</span>
+            </div>
+          </div>
+
+          <form className='quiz__answer' onSubmit={handleSubmit}>
+            {
+              <input
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setAnswer(e.target.value)
+                }
+                className='quiz__answer__letter'
+                type='text'
+                ref={inputRef}
+                placeholder='Réponse'
+                value={answer}
+              />
+            }
+            <div className='quiz__buttons'>
+              <button className='quiz__buttons__button' onClick={handlePass}>
+                Passer
+              </button>
+              <button className='quiz__buttons__button'>Valider</button>
+            </div>
+          </form>
+          {showResult && (
+            <div className='quiz__result'>
+              <div className='quiz__result__content'>
+                {result && (
+                  <>
+                    <img
+                      className='quiz__result__content__check'
+                      src={check}
+                      alt='check logo'
+                    />
+                    <h3 className='quiz__result__content__correct'>
+                      Félicitations !
+                    </h3>
+                    <span className='quiz__result__content__text'>
+                      La réponse est:
+                    </span>
+                  </>
+                )}
+                {!result && (
+                  <>
+                    <div className='quiz__result__content__cross'>x</div>
+                    <h3 className='quiz__result__content__incorrect'>
+                      Dommage
+                    </h3>
+                    <span className='quiz__result__content__text'>
+                      La réponse était:
+                    </span>
+                  </>
+                )}
+                <span className='quiz__result__content__solution'>
+                  {solution}
+                </span>
+                <button
+                  className='quiz__result__content__next'
+                  onClick={handleNextFlag}
+                >
+                  Drapeau suivant
+                  <img src={chevron_right} alt='chevron' />
+                </button>
+              </div>
+            </div>
+          )}
+          {gameOver && (
+            <div className='quiz__gameover'>
+              <div className='quiz__gameover__content'>
+                <h2 className='quiz__gameover__content__title'>
+                  partie terminée
+                </h2>
+                <span className='quiz__gameover__content__score'>
+                  Votre score est: {score}
+                </span>
+                <div className='quiz__gameover__content__buttons'>
+                  <button
+                    className='quiz__gameover__content__buttons__play'
+                    onClick={handlePlayAgain}
+                  >
+                    Rejouer
+                  </button>
+                  <Link
+                    className='quiz__gameover__content__buttons__quit'
+                    to='/'
+                  >
+                    Quitter
+                  </Link>
+                </div>
+              </div>
+            </div>
+          )}
+
+          <Footer />
+        </>
+      )}
     </div>
   );
 };
